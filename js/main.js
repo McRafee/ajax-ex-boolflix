b68a771000fa32eaece5e1470d5667798a277737
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-03-19 18:20:31 +0100

    milestone 2 almost complete

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var source = $("#movie-template").html();
     var movieTemplate = Handlebars.compile(source);
 
-    $("#search").click(function(event){
+    $("#search").click(function(event) {
         var searchedInput = $("#search-input").val().toLowerCase();
         var encodedInput = encodeURI(searchedInput)
         console.log(searchedInput);
@@ -22,7 +22,7 @@ $(document).ready(function() {
                 language: 'it-IT'
             },
             method: 'GET',
-            success: function (data) {
+            success: function(data) {
                 console.log(data);
                 var movies = data.results;
                 for (var i = 0; i < movies.length; i++) {
@@ -32,48 +32,46 @@ $(document).ready(function() {
                         title: movie.title,
                         original_title: movie.original_title,
                         original_language: movie.original_language,
-                        vote_average: movie.vote_average
+                        vote_average: movie.vote_average,
+                        vote_stars: stars(movie.vote_average)
                     }
                     var templateWithData = movieTemplate(movieInformation);
                     $('.movies-container').append(templateWithData);
                 }
             },
-            error: function (err) {
+            error: function(err) {
                 alert('Error');
             }
         });
     });
 
-
-    // var apiBaseUrl = 'https://api.themoviedb.org/3';
-    // $.ajax({
-    //     url: apiBaseUrl + '/search/movie',
-    //     data: {
-    //         api_key: '40d241ffdfaeaa669119b9a7a36e648c',
-    //         query: 'ritorno al futuro',
-    //         language: 'it-IT'
-    //     },
-    //     method: 'GET',
-    //     success: function (data) {
-    //         console.log(data);
-    //         var movies = data.results;
-    //         for (var i = 0; i < movies.length; i++) {
-    //             var movie = movies[i];
-    //             console.log(movie.title);
-    //             var movieInformation = {
-    //                 title: movie.title,
-    //                 original_title: movie.original_title,
-    //                 original_language: movie.original_language,
-    //                 vote_average: movie.vote_average
-    //             }
-    //             var templateWithData = movieTemplate(movieInformation);
-    //             $('.movies-container').append(templateWithData);
-    //         }
-    //     },
-    //     error: function (err) {
-    //         alert('Error');
-    //     }
-    // });
-
+    function stars(vote) {
+        var stars = Math.ceil(vote / 2);
+        var star;
+        switch (stars) {
+            case 0:
+                star = '&star;&star;&star;&star;&star;';
+                break;
+            case 1:
+                star = '&starf;&star;&star;&star;&star;';
+                break;
+            case 2:
+                star = '&starf;&starf;&star;&star;&star;';
+                break;
+            case 3:
+                star = '&starf;&starf;&starf;&star;&star;';
+                break;
+            case 4:
+                star = '&starf;&starf;&starf;&starf;&star;';
+                break;
+            case 5:
+                star = '&starf;&starf;&starf;&starf;&starf;';
+                break;
+            default:
+                star = "N/A";
+        }
+        console.log(stars);
+        return star;
+    }
 
 });
