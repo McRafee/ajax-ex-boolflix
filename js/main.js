da398e6fe4e646b0bfc4c98742cb1f154efd22c7
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-04-03 22:19:43 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
     var source = $("#movie-template").html();
     var movieTemplate = Handlebars.compile(source);
 
+    var source2 = $("#person-template").html();
+    var personTemplate = Handlebars.compile(source2);
+
     $("#search").click(function(event) {
         var searchedInput = $("#search-input").val().toLowerCase();
-        var encodedInput = encodeURI(searchedInput)
+        var encodedInput = encodeURI(searchedInput);
         console.log(searchedInput);
         console.log(encodedInput);
         $('.movies-container').html(""); //clear html
@@ -15,11 +18,11 @@ $(document).ready(function() {
 
         var apiBaseUrl = 'https://api.themoviedb.org/3';
         $.ajax({
-            url: apiBaseUrl + '/search/movie',
+            url: apiBaseUrl + '/search/multi',
             data: {
                 api_key: '40d241ffdfaeaa669119b9a7a36e648c',
                 query: searchedInput,
-                language: 'it-IT'
+                language: 'en'
             },
             method: 'GET',
             success: function(data) {
@@ -29,14 +32,22 @@ $(document).ready(function() {
                     var movie = movies[i];
                     console.log(movie.title);
                     var movieInformation = {
-                        title: movie.title,
+                        title: movie.title || movie.name,
                         original_title: movie.original_title,
                         original_language: movie.original_language,
                         vote_average: movie.vote_average,
-                        vote_stars: stars(movie.vote_average)
+                        vote_stars: stars(movie.vote_average),
+                        image: movie.profile_path || movie.poster_path,
+                        media_type: movie.media_type
                     }
-                    var templateWithData = movieTemplate(movieInformation);
-                    $('.movies-container').append(templateWithData);
+                    if ((movieInformation.media_type == "movie") || (movieInformation.media_type == "tv" )) {
+                        var templateWithData = movieTemplate(movieInformation);
+                        $('.movies-container').append(templateWithData);
+                    } else {
+                        var templateWithData = personTemplate(movieInformation);
+                        $('.movies-container').append(templateWithData);
+                    }
+
                 }
             },
             error: function(err) {
